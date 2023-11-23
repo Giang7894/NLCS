d5a039372b953b2effb79dc6096c0ee85b309695
Giang <giangb2014651@student.ctu.edu.vn>
2023-11-23 11:09:58 +0700

    nav

--- source/public/admin/nav.php
+++ source/public/admin/nav.php
@@ -1,34 +1,171 @@
 <!DOCTYPE html>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" >ADMIN PANEL</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="view_user.php">USER</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="view_comment.php">COMMENT</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                ACTION
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="add_recipe.php">ADD RECIPES</a></li>
-                                <li><a class="dropdown-item" href="view_recipes.php">VIEW RECIPES</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item position-absolute top-25 end-0">
-                            <a class="nav-link active" href="logout.php" class="btn btn-danger" role="button">logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<header>
+  <!-- Sidebar -->
+  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+    <div class="position-sticky">
+      <div class="list-group list-group-flush mx-3 mt-4">
+        <!-- Collapse 1 -->
+        <a
+          class="list-group-item list-group-item-action py-2 ripple"
+          aria-current="true"
+          data-bs-toggle="collapse"
+          href="#collapseExample1"
+          aria-expanded="true"
+          aria-controls="collapseExample1"
+        >
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Expanded menu</span>
+        </a>
+        <!-- Collapsed content -->
+        <ul id="collapseExample1" class="collapse show list-group list-group-flush">
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+        </ul>
+        <!-- Collapse 1 -->
+
+        <!-- Collapse 2 -->
+        <a
+          class="list-group-item list-group-item-action py-2 ripple"
+          aria-current="true"
+          data-bs-toggle="collapse"
+          href="#collapseExample2"
+          aria-expanded="true"
+          aria-controls="collapseExample2"
+        >
+          <i class="fas fa-chart-area fa-fw me-3"></i><span>Collapsed menu</span>
+        </a>
+        <!-- Collapsed content -->
+        <ul id="collapseExample2" class="collapse list-group list-group-flush">
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+          <li class="list-group-item py-1">
+            <a href="" class="text-reset">Link</a>
+          </li>
+        </ul>
+        <!-- Collapse 2 -->
+      </div>
+    </div>
+  </nav>
+  <!-- Sidebar -->
+
+  <!-- Navbar -->
+  <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="#">
+        <img
+          src="/../img/logo.jpg"
+          height="25"
+          alt="MDB Logo"
+          loading="lazy"
+        />
+      </a>
+      <!-- Search form -->
+
+      <!-- Right links -->
+      <ul class="navbar-nav ms-auto d-flex flex-row">
+        <!-- Avatar -->
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center"
+            href="#"
+            id="navbarDropdownMenuLink"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img
+              src="https://img.freepik.com/premium-vector/male-avatar-icon-unknown-anonymous-person-default-avatar-profile-icon-social-media-user-business-man-man-profile-silhouette-isolated-white-background-vector-illustration_735449-122.jpg"
+              class="rounded-circle"
+              height="22"
+              alt="Avatar"
+              loading="lazy"
+            />
+          </a>
+          <ul
+            class="dropdown-menu dropdown-menu-end"
+            aria-labelledby="navbarDropdownMenuLink"
+          >
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
+</header>
+ <style>
+    body {
+  background-color: #fbfbfb;
+}
+@media (min-width: 991.98px) {
+  main {
+    padding-left: 240px;
+  }
+}
+
+/* Sidebar */
+.sidebar {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  padding: 58px 0 0; /* Height of navbar */
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
+  width: 240px;
+  z-index: 600;
+}
+
+@media (max-width: 991.98px) {
+  .sidebar {
+    width: 100%;
+  }
+}
+.sidebar .active {
+  border-radius: 5px;
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+}
+
+.sidebar-sticky {
+  position: relative;
+  top: 0;
+  height: calc(100vh - 48px);
+  padding-top: 0.5rem;
+  overflow-x: hidden;
+  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+}
+ </style>
+
+
+
